45fb2ae6405155724a05e8ef566b8511fe1ab7d1
Zulnurain <meet.zulnurain@gmail.com>
2025-02-03 16:20:34 +0500

    test

--- app/(dashboard)/pages/user/viewalluser/page.js
+++ app/(dashboard)/pages/user/viewalluser/page.js
@@ -8,7 +8,13 @@ const ViewAllUsers = () => {
     const [loading, setLoading] = useState(true);
     const fetchallusers = async () => {
         try {
-            const response = await axios.get(`${process.env.NEXT_PUBLIC_HOST}/api/user/viewalluser`);
+            // const response = await axios.get(`${process.env.NEXT_PUBLIC_HOST}/api/user/viewalluser`);
+            const response = await fetch(`${process.env.NEXT_PUBLIC_HOST}/api/user/viewalluser`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
                 // console.log(response.data);
             if (response.data.status === "success") {
                 setUsers(response.data.data); 
