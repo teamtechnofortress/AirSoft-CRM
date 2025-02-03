b24e73f36db758336c1a385fcef152063ef4fe4b
Zulnurain <meet.zulnurain@gmail.com>
2025-02-03 15:10:49 +0500

    same file

--- app/helper/woocommerce.js
+++ app/helper/woocommerce.js
@@ -13,8 +13,8 @@ export default api;
 
 
 // const api = new WooCommerceRestApi({
-//   url: "https://codedigi.net/",
-//   consumerKey: "ck_80342a68f75bdcbf962b234a94d23affd5de1ca8",
-//   consumerSecret: "cs_d18bbea899a873da84694673efbd20e2fa7bc0e8",
-//   version: "wc/v3"
+//   url: process.env.WOOCOMMERCE_STORE_URL,
+//   consumerKey: process.env.WOOCOMMERCE_CONSUMER_KEY,
+//   consumerSecret: process.env.WOOCOMMERCE_CONSUMER_SECRET,
+//   version: process.env.VERSION,
 // });
