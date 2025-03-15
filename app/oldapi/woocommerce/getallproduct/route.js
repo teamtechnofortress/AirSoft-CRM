cd7bc838c5838813b1ce2212eb62cb8fb2ad056f
Zulnurain <meet.zulnurain@gmail.com>
2025-03-15 15:48:36 +0500

    ggg

--- app/oldapi/woocommerce/getallproduct/route.js
+++ app/oldapi/woocommerce/getallproduct/route.js
@@ -31,7 +31,7 @@ export async function GET(req) {
 
     // ✅ Fetch ONLY 100 products (first page)
     const response = await WooCommerc.get("products", {
-      per_page: 50, // Max WooCommerce allows per request
+      per_page: 20, // Max WooCommerce allows per request
       page: page,        // First page only
     });
 
